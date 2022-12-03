8aef7f08db825a169e5d7601107b78d6736e3d97
Víctor Fernández Flores <victorfernandezflores2@gmail.com>
2022-12-03 11:56:26 +0100

    Refactor: Delete unnecessary comments.

--- Day_2.js
+++ Day_2.js
@@ -14,20 +14,8 @@ function chechDays(date, year){
     return 0;
 }
 
+// -- Test -- 
 const year = 2024
 const holidays = ['02/29', '01/07',] // formato MM/DD
 console.log(countHours(year, holidays));
 
-// 01/06 es el 6 de enero, jueves. Cuenta.
-// 04/01 es el 1 de abril, un viernes. Cuenta.
-// 12/25 es el 25 de diciembre, un domingo. No cuenta.
- // 2 días -> 4 horas extra en el año
-/* const birthday = new Date('12/29/2022');
-const day1 = birthday.getDay(); */
-//console.log(day1);
-
-/* let date2 = "11/09";
-let year2 = 2022;
-let tocotoco = new Date (date2+"/"+year);
-let tabaco = tocotoco.getDay(); 
-console.log(tocotoco.getDay());*/
